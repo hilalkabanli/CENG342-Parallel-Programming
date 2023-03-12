6aedeb324b61b56cb3a2a61ff9308394a75c6e99
hilalokko <hilal.kabanlii@gmail.com>
2023-03-12 14:17:07 +0300

    first

--- hw-1/20050111070.c
+++ hw-1/20050111070.c
@@ -1,7 +1,8 @@
 #include <hellomake.h>
 
-int main(){
-    //call a function in another file
-    myprintHelloMake();
-    return(0);
+int main() {
+  // call a function in another file
+  myPrintHelloMake();
+
+  return(0);
 }
